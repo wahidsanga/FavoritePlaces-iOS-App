54683762f073e5e8f5773323debe7307eb4681ca
Waheed Hasan <waheed.hasan@griffithuni.edu.au>
2023-04-29 01:42:43 +1000

    Initial Commit

---
+++ Assignment2Waheed/Assignment2WaheedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2WaheedApp.swift
+//  Assignment2Waheed
+//
+//  Created by Waheed Hasan on 29/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2WaheedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
